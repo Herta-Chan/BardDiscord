d192a4f9dec0334ae360f5d3fa303bdf0580ba9e
HertaChan <135420571+Herta-Chan@users.noreply.github.com>
2023-06-12 09:08:07 +0700

    Fix BardAI command

--- commands/Info/ask.js
+++ commands/Info/ask.js
@@ -1,3 +1,4 @@
+/*
 // Import Package
 const { MessageAttachment, EmbedBuilder } = require('discord.js');
 require('dotenv');
@@ -63,6 +64,64 @@ module.exports = {
             return send(message, result);
         } catch (e) {
             console.log(e);
+        }
+    },
+};
+*/
+
+// Import Package
+const { MessageAttachment , EmbedBuilder } = require('discord.js')
+require('dotenv')
+async function getBard() {
+    const { Bard } = await import('googlebard');
+    return Bard;
+}
+
+async function main() {
+    const Bard = await getBard();
+    const bot = new Bard(`__Secure-1PSID=${process.env.COOKIE}`, {protocol: "https"});
+    return bot;
+}
+
+// Bard Function
+async function send(message, result) {
+  function reply(content) {
+    message.reply(content);
+  }
+  const embeds = [];
+  let tryCount = 3;
+  while (result.length > 0 && tryCount > 0) {
+    try {
+      let end = Math.min(5000, result.length);
+      embeds.push(new EmbedBuilder().setDescription(`${result.slice(0, end)}`));
+      result = result.slice(end, result.length);
+    } catch (e) {
+      tryCount--;
+      console.log(e);
+    }
+  }
+  if (embeds.length > 8) {
+    reply({files: [new MessageAttachment(Buffer.from(result, "utf-8"), "message.txt")]});
+  } else {
+    reply({embeds,});
+  }
+}
+// Command
+module.exports = {
+    name: 'ask',
+    category: 'Info',
+    description: 'Hỏi đáp với Bard (Google)',
+    aliases: ['bard'],
+    usage: '<PREFIX>ask [message]',
+    run: async (client, message, args) => {
+        const content = message.content.slice(5);
+        if (!content) return message.reply('Không có câu hỏi');
+        const bot = await main();
+        try {
+          const result = await bot.ask(content);
+          await send(message, result);
+        } catch (e) {
+          console.log(e);
         }
     },
 };
